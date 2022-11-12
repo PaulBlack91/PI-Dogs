3bc5416cb851f55a2e06ee5d7d626c6dd5309fe1
Paul Esperon <paul.esperonarg@hotmail.com>
2022-11-12 20:06:17 -0300

    primero

---
+++ api/src/models/Temperament.js
@@ -0,0 +1,18 @@
+const sequelize = require('sequelize');
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('temperament', {
+
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+    },
+        {
+            timestamps: fasle
+        }
+    )
+}
---
+++ api/src/routes/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const Dog = require('./dogs');
+const Temperament = require('./temperaments.js')
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+
+router.use('./dogs', Dog)
+router.use('/temperaments', Temperament)
+
+module.exports = router;
